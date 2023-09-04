70c532d53df8cd56d3ca25d3a48eb1130287e9cd
eagermeagre <134546608+eagermeagre@users.noreply.github.com>
2023-09-05 01:43:52 +0530

    Update search_time.m

--- search_time.m
+++ search_time.m
@@ -3,7 +3,7 @@ files = dir('*.mat');
 for file = files'
     csv = load(file.name);
     %n=length(csv.x_nor);
+    %converting frames to seconds by dividing ny fps
     searchtime=length(csv.x_walk)/40;
-    %searchtime=length(csv.x_walk)/40;
     save((file.name), 'searchtime','-append');
 end
