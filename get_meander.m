3697616f0caf722f7979896198798b5b9f8b9d4a
eagermeagre <134546608+eagermeagre@users.noreply.github.com>
2023-09-05 01:45:03 +0530

    Update get_meander.m

--- get_meander.m
+++ get_meander.m
@@ -2,6 +2,6 @@ files = dir('*.mat');
 
 for file = files'
     csv = load(file.name);
-    meander=1-(csv.r/csv.pathlength);
+    meander=1-(csv.r/csv.pathlength); %r is the beeline of the path
     save((file.name),'meander','-append');
 end
