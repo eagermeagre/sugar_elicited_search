0fe3595f15c587a2df09dc045fd4c6cbdbed680e
eagermeagre <134546608+eagermeagre@users.noreply.github.com>
2023-09-05 01:42:51 +0530

    Update path_length.m

--- path_length.m
+++ path_length.m
@@ -4,7 +4,7 @@ for file = files'
     csv = load(file.name);
     euc_dist=(sqrt(diff(csv.x_walk(:)).^2 + diff(csv.y_walk(:)).^2));
     distance=sum(euc_dist);
-    %converting pix to mm bee 4.09
+    %converting pix to mm 
     pathlength=distance/5.67;
     
     
